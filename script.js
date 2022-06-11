e9c763a591e322b397b845d0c262d42389d273e0
savioferrazgit config --global user.name savioferrazclearclsgit config --global user.name savioferrazgit config --global user.name savioferraz <savio.ferraz88@gmail.com>
2022-06-11 03:16:47 -0300

    Versão 3.0: Adicionada função de selecionar

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-function selecionar() {
-    let itens = document.querySelector(".itens");
-    itens.classList.toggle("selecionado");
-    let icone = document.querySelector(".icone");
-    icone.classList.toggle("escondido");
+function selecionarprato(item) {
+    let escolha = document.querySelector(".cardapiocomida .selecionado");   
+    if (escolha !== null) {
+        escolha.classList.remove("selecionado");
+    }
+    item.classList.add("selecionado");
+}
+function selecionarbebida(item) {
+    let escolha = document.querySelector(".cardapiobebida .selecionado");   
+    if (escolha !== null) {
+        escolha.classList.remove("selecionado");
+    }
+    item.classList.add("selecionado");
+}
+function selecionarsobremesa(item) {
+    let escolha = document.querySelector(".cardapiosobremesa .selecionado");   
+    if (escolha !== null) {
+        escolha.classList.remove("selecionado");
+    }
+    item.classList.add("selecionado");  
+}
+function checkprato(item){
+    let icone = item.querySelector(".cardapiocomida .icone");
+    let check = document.querySelector(".cardapiocomida .escondido")
+    if (check !== null) {
+        icone.classList.add("escondido");
+    }
+        icone.classList.remove("escondido");
 }
